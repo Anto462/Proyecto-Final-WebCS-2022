f18b636d767ca877b1a7722b9f76fc701ecb88b8
Anto462 <0anthony0402@gmail.com>
2022-08-11 19:41:18 -0600

    diseño registro y lista

--- lista.php
+++ lista.php
@@ -14,12 +14,61 @@ $row=mysqli_fetch_array($query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link href="css/styles.css" rel="stylesheet">
-        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="Css/normalize.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
+    <link rel="stylesheet" href="Css/styles.css">
+    <link rel="icon" href="images/image_processing20200511-27037-1qp07kw.png" type="image/x-icon">
     <title>Document</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css" />
 </head>
 <body>
-<div class="container mt-5">
+<header th:fragment="header" class="header">  
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark">
+            <div class="container-fluid">
+              <a class="navbar-brand" href="index.html"> <img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/54336/hammer-and-wrench-emoji-clipart-md.png" width="40" height="40" class="d-inline-block align-top" alt=""/>  Venta De Repuestos</a>
+              <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                <span class="navbar-toggler-icon"></span>
+              </button>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                  <li class="nav-item">
+                    <a class="nav-link active" aria-current="page" href="index.html" ><img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56482/gear-emoji-clipart-md.png" width="25" height="25" class="d-inline-block align-top" alt=""/>  Inicio</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="sobrenosotros.html"><img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56482/gear-emoji-clipart-md.png" width="25" height="25" class="d-inline-block align-top" alt=""/>  Mas sobre nosotros</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="productos.html"><img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56482/gear-emoji-clipart-md.png" width="25" height="25" class="d-inline-block align-top" alt=""/>  Nuestros productos</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="login.html"><img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56482/gear-emoji-clipart-md.png" width="25" height="25" class="d-inline-block align-top" alt=""/> Login</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="registro.php"><img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56482/gear-emoji-clipart-md.png" width="25" height="25" class="d-inline-block align-top" alt=""/> Registrarse</a>
+                  </li>
+                  <li class="nav-item">
+                    <a class="nav-link active" href="lista.php"><img src="https://creazilla-store.fra1.digitaloceanspaces.com/emojis/56482/gear-emoji-clipart-md.png" width="25" height="25" class="d-inline-block align-top" alt=""/> Lista de Registrados</a>
+                  </li>
+                </ul>
+              </div>
+              <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                <ul class="navbar-nav me-auto mb-2 mb-lg-0">
+                <li class="nav-item">
+                  <a class="nav-link nav-derecha"> Numero: +506 2233-4455</a>
+                </li>
+                <li class="nav-item">
+                  <a class="nav-link nav-derecha"> Mail: Soporterep@Repuestos.co.cr</a>
+                </li>
+                </ul>
+              </div>
+            </div>
+          </nav>
+    </header>
+    <br>
+    <div class="sobrenoshead centrar">
+    <h1>Usuarios registrados</h1>
+    </div>
+    <div class="container mt-5 centrar" >
                    
 
                         <div class="col-md-8">
@@ -65,6 +114,8 @@ $row=mysqli_fetch_array($query);
                         
                     </div>  
             </div>
-            <button> <a href="index.html">Volver a la pagina principal</a></button>
+            <div class="centrar">
+            <button> <a href="index.html" style="text-decoration: none; color: white;"><span></span> Volver a la pagina principal</a></button>
+            </div>
 </body>
 </html>
--- registro.php
+++ registro.php
@@ -14,15 +14,23 @@ $row=mysqli_fetch_array($query);
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="Css/normalize.css">
     <link href="css/style.css" rel="stylesheet">
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.0.0-beta1/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-giJF6kkoqNQ00vy+HMDP7azOuL0xtbfIcaT9wjKHr8RbDVddVHyTfAAsrekwKmP1" crossorigin="anonymous">
     <title>Document</title>
 </head>
 <body>
 <div class="container mt-5">
+<div class="container loginbox">
                     <div class="row"> 
                         
                         <div class="col-md-3">
+                        <div class="col-lg-5">
+                        <img src="images/1001384213.jpg " class="img-fluid" style="border-radius: 33px 33px 33px 33px;
+                        -moz-border-radius: 33px 33px 33px 33px;
+                        -webkit-border-radius: 33px 33px 33px 33px;
+                         border: 0px solid #000000;">
+                         </div>
                             <h1>Registrarse</h1>
                             <form action="insertar.php" method="POST">
                             <input type="text" class="form-control mb-3" name="CEDULA_CLI" placeholder="CEDULA_CLI">
@@ -35,12 +43,13 @@ $row=mysqli_fetch_array($query);
                                     <input type="password" class="form-control mb-3" name="CEDULA_EMP" placeholder="CEDULA_EMP">
                                     
 
-                                    <input type="submit" href="index.html" class="btn btn-danger">
+                                    <input type="submit" href="index.html" class="btn btn-dark">
                                 </form>
                         </div>
 
                        
                     </div>  
+                    </div>
             </div>
 </body>
 </html>
