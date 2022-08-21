a35154d08ecaede8617c88f8d313744e049fb61e
Anto462 <0anthony0402@gmail.com>
2022-08-21 13:54:38 -0600

    Ajustes mostrar update

--- actualizaProductos.php
+++ actualizaProductos.php
@@ -22,6 +22,7 @@ $row=mysqli_fetch_array($query);
         
     </head>
     <body>
+        
                 <div class="container mt-5">
                     <form action="updateProductos.php" method="POST">
                    
